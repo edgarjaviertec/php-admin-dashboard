7477b907052cad53d9062943f53a0f17839dce87
Edgar Tec <edgartec87@gmail.com>
2020-11-09 12:17:36 -0500

    Se agregó una función a los helpers para obtener las configuraciones generales

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,18 @@
 <?php
 
+if (!function_exists('config')) {
+    function config($key, $fallback = null)
+    {
+        static $config;
+        if (is_null($config)) {
+            $config = include 'configs.php';
+        }
+        return array_key_exists($key, $config)
+            ? $config[$key]
+            : $fallback;
+    }
+}
+
 if (!function_exists('env')) {
     function env($key, $fallback = null)
     {
