0936e54a53ece359ae48c24bad64bf76ff58a9a5
Edgar Tec <edgartec87@gmail.com>
2020-11-09 11:55:35 -0500

    Se creó un archivo de helpers con una función para poder leer las variables de entorno

---
+++ app/helpers.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!function_exists('env')) {
+    function env($key, $fallback = null)
+    {
+        $env_array = $_ENV;
+        $value = null;
+        if (array_key_exists($key, $env_array)) {
+            $value = $env_array[$key];
+            if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
+                $value = $matches[2];
+            }
+            switch (strtolower($value)) {
+                case 'true':
+                case '(true)':
+                    $value = true;
+                    break;
+                case 'false':
+                case '(false)':
+                    $value = false;
+                    break;
+                case 'empty':
+                case '(empty)':
+                    $value = '';
+                    break;
+                case 'null':
+                case '(null)':
+                    $value = null;
+                    break;
+            }
+            return $value;
+        } else {
+            return $fallback;
+        }
+    }
+}
